c08927c5300dbcc2c581fe0909d13296b22d6ef4
circuitpotato <135819236+circuitpotato@users.noreply.github.com>
2023-06-28 17:56:58 +0800

    Update truth_table_solver_2.0.py

--- truth_table_solver_2.0.py
+++ truth_table_solver_2.0.py
@@ -3,31 +3,33 @@ from sympy import simplify_logic, sympify
 
 # Global variables
 truth_table_input = None
-truth_table_input_max = 6
+truth_table_input_max = 9   # maximum no. of truth table inputs
 truth_table_symbols = None
 button_values = []
 
-# GUI global size
+# truth table GUI/labels global size
 label_width = 27
 label_pad_x = 13
 label_pad_y = 15
 font_size = 10
 
+# output GUI/labels global size
+output_label_width = 59
+output_label_padx = 10
+output_label_pady = 10
 
-# submit button flag variable
-submit1_pressed = False
-submit2_pressed = False
+# copy button global size
+button_padx = 10
+button_pady = 5
 
 
 def button2_pressed():
-
     # Configure row and column weights for expansion
     canvas2.yview_moveto(0)  # Set the initial view position of the canvas to the top
     canvas2.grid(row=5, column=0, columnspan=3, sticky="nsew")
     scrollbar2.grid(row=5, column=3, sticky="nsew")
     canvas2.create_window((0, 0), window=content_frame2)
 
-    global submit2_pressed
     button_values_list = [int(item) if item in ['1'] else item for item in button_values]
     my_simplified_expression = get_simplified_expression(truth_table_symbols, str(truth_table_input), button_values_list)
     get_verilog_output(my_simplified_expression)
@@ -35,8 +37,9 @@ def button2_pressed():
 
 
 def get_inputs():
-    global truth_table_input, truth_table_symbols, button_values, submit1_pressed, submit2_pressed
+    global truth_table_input, truth_table_symbols, button_values
 
+    # get inputs from user
     input_value = number_entry.get()
     symbols_value = string_entry.get()
 
@@ -46,7 +49,6 @@ def get_inputs():
         truth_table_symbols = symbols_value
         print_truth_table(truth_table_input, truth_table_symbols)
         button_values = create_toggle_buttons(truth_table_input)
-        submit1_pressed = True  # Set the flag to indicate button has been pressed
 
     else:
         messagebox.showerror("Error", "Invalid input! \na) Truth Table Inputs: Enter a number between 2 to " + str(truth_table_input_max) + " \nb) Truth Table Symbols: Enter with respect to the number of inputs (E.g. ABCD, DCBA, aAc, cba)")
@@ -104,7 +106,7 @@ def create_toggle_buttons(num_buttons):
         # Update the list with the button's current value
         button_values_final[buttons.index(my_button)] = my_button.cget("text")
 
-    # display truth table output buttons
+    # display truth table output buttons for user to toggle
     buttons = []
     for i in range(2**num_buttons):
         # whitespace = Label(content_frame, text=f"", padx=10, pady=5)
@@ -117,12 +119,8 @@ def create_toggle_buttons(num_buttons):
         button_values_final.append("0")  # Initialize the list with "0" values
 
     # Show truth table output submit button
-    output_submit_button = Button(root, text="Submit", padx=10, pady=5, command=button2_pressed)
+    output_submit_button = Button(root, text="Submit", padx=button_padx, pady=button_pady, command=button2_pressed)
     output_submit_button.grid(row=4, column=2, sticky="nsew")
-    #
-    # # Show Submit Button
-    # BUTTON = Button(root, text=f"Submit", command=get_inputs, padx=10, pady=5)
-    # BUTTON.grid(row=1, column=2, sticky="nsew")
 
     # Return the button values
     return button_values_final
@@ -130,7 +128,7 @@ def create_toggle_buttons(num_buttons):
 
 # this function splits a string if it exceeds a certain length
 def split_string(sentence_str):
-    string_length = 80
+    string_length = 70
     if len(sentence_str) <= string_length:
         return sentence_str  # Return the original string as a single element list
 
@@ -180,60 +178,61 @@ def get_simplified_expression(input_symbols, tt_inputs, tt_outputs):
         simplified_expression_dont_care = simplify_logic(simplified_expression, dontcare=expression_dont_care)
         simplified_expression_dont_care = str(simplified_expression_dont_care)
         final_simplified_expression = simplified_expression_dont_care
-        # print("Simplified Boolean Expression dont care: Z = " + simplified_expression_dont_care)
 
-        # simplified equivalent dont care
-        simplified_label1 = Label(content_frame2, text="Simplified Boolean Expression dont care: ")
+        # simplified equivalent dont care (title)
+        simplified_label1 = Label(content_frame2, text=f"Simplified Boolean Expression dont care: ")
         simplified_label1.grid(row=0, column=0, sticky="nsew")
         simplified_label1.configure(font=("Arial", font_size, "bold italic"), foreground="brown")
 
-        simplified_dont_care_output = "Z = " + simplified_expression_dont_care
+        # simplified equivalent dont care (output expression)
+        simplified_dont_care_output = f"Z = {simplified_expression_dont_care}"
         split_simplified_dont_care_output = split_string(simplified_dont_care_output)
-        simplified_label2 = Label(content_frame2, text=split_simplified_dont_care_output, width=59, padx=10, pady=10, relief="solid")
+        simplified_label2 = Label(content_frame2, text=split_simplified_dont_care_output, width=output_label_width, padx=10, pady=10, relief="solid")
         simplified_label2.grid(row=1, column=0, sticky="we")
         simplified_label2.configure(font=("Arial", font_size, "bold"), foreground="black")
 
         # copy button
-        simplified_copy_button = Button(content_frame2, text=f"copy", command=lambda: copy_to_clipboard(simplified_dont_care_output), padx=10, pady=5)
+        simplified_copy_button = Button(content_frame2, text=f"copy", command=lambda: copy_to_clipboard(simplified_dont_care_output), padx=button_padx, pady=button_pady)
         simplified_copy_button.grid(row=2, column=0, sticky="e")
 
     else:
         simplified_expression = str(simplified_expression)
         final_simplified_expression = simplified_expression
-        # print("Simplified Boolean Expression: Z = " + simplified_expression)
 
-        # simplified equivalent
+        # simplified equivalent (title)
         simplified_label1 = Label(content_frame2, text="Simplified Boolean Expression: ")
         simplified_label1.grid(row=0, column=0, sticky="nsew")
         simplified_label1.configure(font=("Arial", font_size, "bold italic"), foreground="brown")
 
+        # simplified equivalent (expression)
         simplified_output = "Z = " + simplified_expression
         split_simplified_output = split_string(simplified_output)
-        simplified_label2 = Label(content_frame2, text=split_simplified_output, width=59, padx=10, pady=10, relief="solid")
+        simplified_label2 = Label(content_frame2, text=split_simplified_output, width=output_label_width, padx=10, pady=10, relief="solid")
         simplified_label2.grid(row=1, column=0, sticky="we")
         simplified_label2.configure(font=("Arial", font_size, "bold"), foreground="black")
 
         # copy button
-        simplified_copy_button = Button(content_frame2, text=f"copy", command=lambda: copy_to_clipboard(simplified_output), padx=10, pady=5)
+        simplified_copy_button = Button(content_frame2, text=f"copy", command=lambda: copy_to_clipboard(simplified_output), padx=button_padx, pady=button_pady)
         simplified_copy_button.grid(row=2, column=0, sticky="e")
     return final_simplified_expression
 
 
 # Translate simplified boolean expression to Verilog Syntax
 def get_verilog_output(expression_str):
-    # verilog equivalent
+    # verilog equivalent (title)
     verilog_label1 = Label(content_frame2, text=f"Verilog equivalent: ")
     verilog_label1.grid(row=3, column=0, sticky="nsew")
     verilog_label1.configure(font=("Arial", font_size, "bold italic"), foreground="brown")
 
+    # verilog equivalent (expression)
     verilog_output = "assign Z = " + expression_str + ";"
     split_verilog_output = split_string(verilog_output)
-    verilog_label2 = Label(content_frame2, text=split_verilog_output, width=59, padx=10, pady=10, relief="solid")
+    verilog_label2 = Label(content_frame2, text=split_verilog_output, width=output_label_width, padx=output_label_padx, pady=output_label_pady, relief="solid")
     verilog_label2.grid(row=4, column=0, sticky="we")
     verilog_label2.configure(font=("Arial", font_size, "bold"), foreground="black")
 
     # copy button
-    verilog_copy_button = Button(content_frame2, text=f"copy", command=lambda: copy_to_clipboard(verilog_output), padx=10, pady=5)
+    verilog_copy_button = Button(content_frame2, text=f"copy", command=lambda: copy_to_clipboard(verilog_output), padx=button_padx, pady=button_pady)
     verilog_copy_button.grid(row=5, column=0, sticky="e")
 
 
@@ -252,22 +251,20 @@ def get_vhdl_output(expression_str):
     expression_str = expression_str.replace("|", "or")
     expression_str = expression_str.replace("-", "not ")
 
-    # print("VHDL equivalent: ")
-    # print("Z <= " + expression_str + ";")
-    # vhdl equivalent
-
+    # vhdl equivalent (title)
     vhdl_label1 = Label(content_frame2, text=f"VHDL equivalent: ")
     vhdl_label1.grid(row=6, column=0, sticky="nsew")
     vhdl_label1.configure(font=("Arial", font_size, "bold italic"), foreground="brown")
 
+    # verilog equivalent (expression)
     vhdl_output = "Z <= " + expression_str + ";"
     split_vhdl_output = split_string(vhdl_output)
-    vhdl_label2 = Label(content_frame2, text=split_vhdl_output, width=59, padx=10, pady=10, relief="solid")
+    vhdl_label2 = Label(content_frame2, text=split_vhdl_output, width=output_label_width, padx=10, pady=10, relief="solid")
     vhdl_label2.grid(row=7, column=0, sticky="we")
     vhdl_label2.configure(font=("Arial", font_size, "bold"), foreground="black")
 
     # copy button
-    vhdl_copy_button = Button(content_frame2, text=f"copy", command=lambda: copy_to_clipboard("Z <= " + expression_str + ";"), padx=10, pady=5)
+    vhdl_copy_button = Button(content_frame2, text=f"copy", command=lambda: copy_to_clipboard("Z <= " + expression_str + ";"), padx=button_padx, pady=button_pady)
     vhdl_copy_button.grid(row=8, column=0, sticky="e")
 
 
@@ -277,8 +274,8 @@ def copy_to_clipboard(text):
     messagebox.showinfo("Copied!", "Text has been copied to the clipboard.")
 
 
+# main code
 root = Tk()
-# Show GUI
 
 # Show truth table inputs GUI (inputs)
 number_label = Label(root, text=f"No. of Truth Table (+ve integers only):", padx=10, pady=5)
@@ -293,19 +290,16 @@ string_entry = Entry(root)
 string_entry.grid(row=1, column=1, padx=10, pady=5, sticky="nsew")
 
 # Show Submit Button
-BUTTON = Button(root, text=f"Submit", command=get_inputs, padx=10, pady=5)
+BUTTON = Button(root, text=f"Submit", command=get_inputs, padx=button_padx, pady=button_pady)
 BUTTON.grid(row=1, column=2, sticky="nsew")
 
-# label = Label(content_frame, text=f"{binary_number}", width=label_width, relief="raised", padx=label_pad_x, pady=label_pad_y, anchor="center")
-# label.grid(row=(5+i), column=0, sticky="nsew")
-
-# Create a Canvas and Scrollbar
+# Create a Canvas and Scrollbar (only displayed after submit button)
 canvas = Canvas(root)
 canvas2 = Canvas(root)
 
+# generate scrollbars
 scrollbar = Scrollbar(root, orient="vertical", command=canvas.yview)
 scrollbar2 = Scrollbar(root, orient="vertical", command=canvas2.yview)
-
 canvas.configure(yscrollcommand=scrollbar.set)
 canvas2.configure(yscrollcommand=scrollbar2.set)
 
@@ -325,6 +319,7 @@ def update_scroll_region2(event):
     canvas2.yview_moveto(0)  # Set the initial view position of the canvas to the top
 
 
+# configure the scrollbar to update properly
 content_frame.bind("<Configure>", update_scroll_region)
 content_frame2.bind("<Configure>", update_scroll_region2)
 
